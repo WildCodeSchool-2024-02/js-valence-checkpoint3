71338a4dd01f27d28b7da10986adb4978c346681
iremchabanne <irem_kcakir@outlook.com>
2024-07-08 10:35:33 +0200

    MDC and step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,15 +1,16 @@
 CREATE TABLE boat (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    coord_x INT NOT NULL,
+    coord_y INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tile (
-  id INT AUTO_INCREMENT NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
+    id INT AUTO_INCREMENT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    coord_x INT NOT NULL,
+    coord_y INT NOT NULL,
+    PRIMARY KEY (id),
+    has_treasure BOOLEAN NOT NULL DEFAULT 0
 );
