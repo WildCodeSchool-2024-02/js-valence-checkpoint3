857ff7a63335a5950e87e82bfad696b09064c691
Nicolas Wormser <nicolas@codewithflair.org>
2024-07-08 15:40:18 +0200

    Step BONUS - Use param to filter boat.readAll()

--- server/database/models/BoatRepository.js
+++ server/database/models/BoatRepository.js
@@ -5,14 +5,25 @@ class BoatRepository extends AbstractRepository {
     super({ table: "boat" });
   }
 
-  async readAll() {
+  async readAll(where) {
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await this.database.query(`
+
+    let query = `
       SELECT b.*, t.type, t.has_treasure
       FROM ${this.table} b
       JOIN tile t
       ON b.coord_x = t.coord_x
-      AND b.coord_y = t.coord_y`);
+      AND b.coord_y = t.coord_y
+    `;
+    const params = [];
+
+    // WHERE b.name = ?
+    if (where && where.name) {
+      query += `WHERE b.name = ?`;
+      params.push(where.name);
+    }
+
+    const [rows] = await this.database.query(query, params);
 
     // Return the array of boats
     return rows;
