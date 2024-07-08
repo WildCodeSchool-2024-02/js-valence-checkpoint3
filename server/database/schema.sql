f0db523a71cfad3fbecc594f4c774e60c7fde06d
FadiAntwan95 <antwan.fadi95@gmail.com>
2024-07-08 09:42:20 +0200

    finished step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -13,3 +13,33 @@ CREATE TABLE tile (
   coord_y INT NOT NULL,
   PRIMARY KEY(id)
 );
+
+
+
+
+
+CREATE TABLE album (
+    id_album INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(25) NOT NULL,
+    genre VARCHAR(25) NOT NULL,
+    image VARCHAR(255),
+    artiste VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE piste (
+    id_piste INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(25) NOT NULL,
+    url_youtube VARCHAR(255) NOT NULL,
+    id_album INT,
+    FOREIGN KEY (id_album) REFERENCES album(id_album)
+);
+
+
+CREATE TABLE contient (
+    id_album INT,
+    id_piste INT,
+    PRIMARY KEY (id_album, id_piste),
+    FOREIGN KEY (id_album) REFERENCES album(id_album),
+    FOREIGN KEY (id_piste) REFERENCES piste(id_piste)
+);
