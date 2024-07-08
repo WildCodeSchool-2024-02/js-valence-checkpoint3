c9f57d159806b1ac0cdbd2b5c588c5bf92d1d778
IChris2611I <pachris2611@gmail.com>
2024-07-08 09:50:39 +0200

    finished step 2

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,13 @@
-module.exports = {};
+
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tile = await tables.tile.readAll();
+    res.status(200).json(tile);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {browse};
