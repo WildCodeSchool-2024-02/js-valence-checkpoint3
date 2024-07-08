e37c10d60cd847edfd6564c8a4db845aa912358f
FadiAntwan95 <antwan.fadi95@gmail.com>
2024-07-08 09:55:22 +0200

    finished step 1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE tile (
   type VARCHAR(255) NOT NULL,
   coord_x INT NOT NULL,
   coord_y INT NOT NULL,
+  has_treasure BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY(id)
 );
 
@@ -18,28 +19,3 @@ CREATE TABLE tile (
 
 
 
-CREATE TABLE album (
-    id_album INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(25) NOT NULL,
-    genre VARCHAR(25) NOT NULL,
-    image VARCHAR(255),
-    artiste VARCHAR(255) NOT NULL
-);
-
-
-CREATE TABLE piste (
-    id_piste INT AUTO_INCREMENT PRIMARY KEY,
-    titre VARCHAR(25) NOT NULL,
-    url_youtube VARCHAR(255) NOT NULL,
-    id_album INT,
-    FOREIGN KEY (id_album) REFERENCES album(id_album)
-);
-
-
-CREATE TABLE contient (
-    id_album INT,
-    id_piste INT,
-    PRIMARY KEY (id_album, id_piste),
-    FOREIGN KEY (id_album) REFERENCES album(id_album),
-    FOREIGN KEY (id_piste) REFERENCES piste(id_piste)
-);
