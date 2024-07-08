98696f69066930f452659c362a0e5634d4eb9b65
Hasbem <marvinmsb69@gmail.com>
2024-07-08 10:01:47 +0200

    step 2

--- server/app/controllers/tileActions.js
+++ server/app/controllers/tileActions.js
@@ -1,1 +1,16 @@
-module.exports = {};
+const tables = require("../../database/tables");
+
+const browse = async (req, res, next) => {
+  try {
+    const tiles = await tables.tile.readAll();
+
+    res.json(tiles);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+module.exports = {
+  browse,
+};
