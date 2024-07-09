77077ce45070a0ba12ead7bcf02bcaa07f7040ca
iremchabanne <irem_kcakir@outlook.com>
2024-07-09 08:44:25 +0200

    step 4

--- server/app/routers/api/boats/router.js
+++ server/app/routers/api/boats/router.js
@@ -5,12 +5,12 @@ const router = express.Router();
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-
+const tileExist = require("../../../services/tileExists");
 const { browse, edit } = require("../../../controllers/boatActions");
 
 router.get("/", browse);
 
-router.put("/:id", edit);
+router.put("/:id", tileExist, edit);
 
 /* ************************************************************************* */
 
--- server/app/services/tileExists.js
+++ server/app/services/tileExists.js
@@ -1,1 +1,18 @@
-module.exports = null;
+const tables = require("../../database/tables");
+
+const tileExists = async (req, res, next) => {
+  const { coord_x: x, coord_y: y } = req.body;
+
+  try {
+    const tiles = await tables.tile.readByCoordinates(x, y);
+    if (tiles.length === 0) {
+      res.sendStatus(422);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = tileExists;
